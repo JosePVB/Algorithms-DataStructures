dced323053f881f6032e8ad8abf1d28993219311
Jose Vargas <jpv.badilla@gmail.com>
2018-04-24 22:17:51 -0400

    Added remaining simple arithmetic operators as well as relational operators and some unary operators.

--- Chapter 1/chapter_1_notes.py
+++ Chapter 1/chapter_1_notes.py
@@ -5,7 +5,6 @@ This script contains the examples developed throughout the chapter 1
 of the 'Problem Solving with Algorithms and Data Structures using Python'
 """
 
-# Fraction class
 
 def gcd(m, n):
     """Returns the greatest common denominator between m and n.
@@ -35,17 +34,28 @@ class Fraction():
         numerator, int
         dnoominator, int
         """
-        self.numerator = numerator
-        self.denominator = denominator
+        if not isinstance(numerator, int) or not isinstance(denominator, int):
+            raise TypeError('Both the numerator and denominator must be ints.')
+
+        if denominator < 0:
+            numerator = -numerator
+            denominator = abs(denominator)
+
+        greatest_common_denominator = abs(gcd(numerator, denominator))
+        self.numerator = numerator // greatest_common_denominator
+        self.denominator = denominator // greatest_common_denominator
 
     def __str__(self):
         """Overwrites defafult __str__() to visually look like a fraction."""
+        if self.denominator == 1:
+            return str(self.numerator)
         return "{}/{}".format(self.numerator, self.denominator)
 
     def __add__(self, other_fraction):
-        """Overwrites the default __add__() for fraction addition.
+        """Implements fraction addition.
 
         General fraction addition:  a/b + c/d = (d*a + c*b) / (b*d)
+
         Variables
         ---------
         other_fraction, Fraction
@@ -53,11 +63,138 @@ class Fraction():
         new_numerator = (self.numerator * other_fraction.denominator
                          + self.denominator * other_fraction.numerator)
         new_denominator = self.denominator * other_fraction.denominator
-        greatest_common_denominator = gcd(new_numerator, new_denominator)
-        return Fraction(new_numerator // greatest_common_denominator,
-                        new_denominator // greatest_common_denominator)
+        return Fraction(new_numerator, new_denominator)
+
+    def __eq__(self, other_fraction):
+        """Implements fraction equality.
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        first_numerator = self.numerator * other_fraction.denominator
+        second_numerator = self.denominator * other_fraction.numerator
+        return first_numerator == second_numerator
+
+    def __sub__(self, other_fraction):
+        """Implements fraction subtraction.
+
+        General fraction subtraction: a/b - c/d = (a * d - c * b) / (b * d)
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        new_numerator = (self.numerator * other_fraction.denominator
+                         - self.denominator * other_fraction.numerator)
+        new_denominator = self.denominator * other_fraction.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __mul__(self, other_fraction):
+        """Implements fraction multiplication.
+
+        General fraction multiplication: a/b * c/d = (a * c) / (b * d)
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        new_numerator = self.numerator * other_fraction.numerator
+        new_denominator = self.denominator * other_fraction.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __truediv__(self, other_fraction):
+        """Implements fraction division.
+
+        General fraction division: a/b / c/d = (a * d) / (b * c)
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        new_numerator = self.numerator * other_fraction.denominator
+        new_denominator = self.denominator * other_fraction.numerator
+        return Fraction(new_numerator, new_denominator)
+
+    def __le__(self, other_fraction):
+        """Implements less than or equal to for fractions.
+
+        General equation: a/b <= c/d -> a*d <= b*c
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        lhs = self.numerator * other_fraction.denominator
+        rhs = self.denominator * other_fraction.numerator
+        return lhs <= rhs
+
+    def __lt__(self, other_fraction):
+        """Implements less than for fractions.
+
+        General equation a/b < c/d -> a*d < b*c
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        lhs = self.numerator * other_fraction.denominator
+        rhs = self.denominator * other_fraction.numerator
+        return lhs < rhs
+
+    def __ge__(self, other_fraction):
+        """Implements greater than or equal to for fractions.
+
+        General equation a/b >= c/d -> a*d >= b*c
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        lhs = self.numerator * other_fraction.denominator
+        rhs = self.denominator * other_fraction.numerator
+        return lhs >= rhs
+
+    def __gt__(self, other_fraction):
+        """Implements greater than for fractions.
+
+        General equation a/b > c/d -> a*d > b*c
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        lhs = self.numerator * other_fraction.denominator
+        rhs = self.denominator * other_fraction.numerator
+        return lhs > rhs
+
+    def __ne__(self, other_fraction):
+        """Implements fraction inequality.
+
+        Variables
+        ---------
+        other_fraction, Fraction
+        """
+        first_numerator = self.numerator * other_fraction.denominator
+        second_numerator = self.denominator * other_fraction.numerator
+        return first_numerator != second_numerator
+
+    def __neg__(self):
+        """Implements unary negation."""
+        return Fraction(-self.numerator, self.denominator)
+
+    def __pos__(self):
+        return self
+
+    def __invert__(self):
+        """Implements unary inversion, i.e. the reciprocal."""
+        return Fraction(self.denominator, self.numerator)
 
 
 if __name__ == "__main__":
-    print(Fraction(3, 5))
-    print(Fraction(2, 5) + Fraction(1, 5))
+    print("Printing: {}".format(Fraction(3, 5)))
+    print("Addition: {}".format(Fraction(2, 5) + Fraction(1, 5)))
+    print("Subtraction: {}".format(Fraction(1, 3) - Fraction(1, 2)))
+    print("Equality: {}".format(Fraction(1, 4) == Fraction(4, 16)))
+    print("Multiplication: {}".format(Fraction(2, 3) * Fraction(6, 7)))
+    print("Division: {}".format(Fraction(3, 4) / Fraction(1, 8)))
