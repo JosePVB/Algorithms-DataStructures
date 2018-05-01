e3c96a0819dc29d8e6608843c5b9774a75923e4f
Jose Vargas <jpv.badilla@gmail.com>
2018-04-30 21:07:16 -0400

    python_data_structures_performance.py tests and compares the performance of common list and dictionary operations on inputs of different magnitudes. kth_smallest_int.py includes 2 algorithms to find the kth smallest number in a list of numbers; the first is O(kn) and the second is O(nlogn).

--- Chapter 2/python_data_structures_performance.py
+++ Chapter 2/python_data_structures_performance.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Script tests the performance of list and dictionary operations."""
+"""
+Script tests the performance of list and dictionary operations for inputs
+of different magnitudes.
+"""
 
 
 def list_concat():
@@ -27,48 +30,7 @@ def list_cast():
 
 
 if __name__ == "__main__":
-    """
-    Here is the final output.
 
-    Testing performance of different list creation methods.
-
-
-    concat:  3.0565415769815445  milliseconds
-    append:  0.17854132698266767  milliseconds
-    comprehension:  0.07684464097837918  milliseconds
-    cast:  0.03300053800921887  milliseconds
-
-
-    Testing performance between popping at first and last index.
-
-
-    Pop at [0] for 1,000 elements:  0.0005668539961334318  milliseconds
-    Pop at [-1] for 1,000 elements:  0.0002143520105164498  milliseconds
-    Pop at [0] for 10,000 elements:  0.006415733019821346  milliseconds
-    Pop at [-1] for 10,000 elements:  0.00021288497373461723  milliseconds
-    Pop at [0] for 100,000 elements:  0.06899165798677132  milliseconds
-    Pop at [-1] for 100,000 elements:  0.000239285989664495  milliseconds
-    Pop at [0] for 1,000,000 elements:  1.3821034340071492  milliseconds
-    Pop at [-1] for 1,000,000 elements:  0.00022224400890991092  milliseconds
-    Pop at [0] for 10,000,000 elements:  15.198659743997268  milliseconds
-    Pop at [-1] for 10,000,000 elements:  0.00022028799867257476  milliseconds
-
-
-    Testing contains performance of lists and dictionaries.
-
-
-    List of 1,000 elements:  0.016853645007358864  milliseconds.
-    Dict of 1,000 elements:  0.002178505004849285  milliseconds.
-    List of 10,000 elements:  0.15340963000198826  milliseconds.
-    Dict of 10,000 elements:  0.0023701569880358875  milliseconds.
-    List of 100,000 elements:  1.519411809014855  milliseconds.
-    Dict of 100,000 elements:  0.0032649300119373947  milliseconds.
-    List of 1,000,000 elements:  15.116391540010227  milliseconds.
-    Dict of 1,000,000 elements:  0.002498740010196343  milliseconds.
-    List of 10,000,000 elements:  148.0063763129874  milliseconds.
-    Dict of 10,000,000 elements:  0.0032509609882254153  milliseconds.
-
-    """
     import timeit
     import random
 
