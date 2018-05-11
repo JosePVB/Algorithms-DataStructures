197db75a67048aff3c262107eaef18ad0bfd5401
Jose Vargas <jpv.badilla@gmail.com>
2018-05-10 22:06:56 -0400

    Implemented a Deque class with dunder methods to make the API more Pythonic than the example in the book. Passed the 7 unit tests.

---
+++ Chapter_3/Deques/Tests/test_deque.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+import unittest
+
+
+import Chapter_3.Deques.deques as deques
+
+
+class TestDeque(unittest.TestCase):
+    """
+    Tests the functionality of the Deque class.
+        -   Creation from lists.
+        -   Creation from genexps.
+        -   Creation from non-iterables.
+
+        -   Addition to the right side of the deque.
+        -   Addition to the left side of the deque.
+
+        -   * operator.
+        -   ** operator.
+    """
+    def setUp(self):
+        """
+        Deques and lists used throughout the test cases.
+        """
+        self.right_appended_deque = deques.Deque()
+        self.left_appended_deque = deques.Deque()
+        self.items = [1, 2, 3]
+        self.additional_items = [4, 5, 6]
+
+        for item in self.items:
+            self.right_appended_deque.append_right(item)
+            self.left_appended_deque.append_left(item)
+
+    def test_deque_instantiation_from_list(self):
+        """
+        Test whether a Deque is the same whether each item is appended one at
+        a time or passed all at once in a list.
+        """
+        self.assertEqual(self.right_appended_deque, deques.Deque(self.items))
+        self.assertEqual(self.left_appended_deque,
+                         deques.Deque(self.items[::-1]))
+
+    def test_deque_instantiation_from_genexp(self):
+        """
+        Test the creation of a Deque through a generator expression.
+        """
+        # Test right append
+        genexp = (item for item in self.items)
+        self.assertEqual(self.right_appended_deque, deques.Deque(genexp))
+
+        # Test left append
+        genexp = (item for item in self.items[::-1])
+        self.assertEqual(self.left_appended_deque, deques.Deque(genexp))
+
+    def test_deque_raises_for_non_iterable(self):
+        """
+        Tests that a Deque can not be created from a non iterable.
+        """
+        with self.assertRaises(TypeError):
+            deques.Deque(1)
+
+    def test_deque_addition_to_right(self):
+        """
+        Tests the result of Deque + iterable
+        """
+        self.assertEqual(self.right_appended_deque + self.additional_items,
+                         deques.Deque(self.items + self.additional_items))
+        self.assertEqual(
+            self.left_appended_deque + self.additional_items,
+            deques.Deque(self.items[::-1] + self.additional_items)
+        )
+
+    def test_deque_addition_to_left(self):
+        """
+        Tests the result of iterable + Deque
+        """
+        self.assertEqual(self.additional_items + self.right_appended_deque,
+                         deques.Deque(self.additional_items + self.items))
+        self.assertEqual(
+            self.additional_items + self.left_appended_deque,
+            deques.Deque(self.additional_items + self.items[::-1])
+        )
+
+    def test_deque__mul__(self):
+        """
+        Tests the result of Deque * n
+        """
+        self.assertEqual(self.right_appended_deque * 2,
+                         deques.Deque(self.items * 2))
+        self.assertEqual(self.left_appended_deque * 3,
+                         deques.Deque(self.items[::-1] * 3))
+
+    def test_deque__pow__(self):
+        """
+        Tests the result of Deque ** n
+        """
+        self.assertEqual(
+            self.right_appended_deque ** 2,
+            deques.Deque([num for num in self.items for _ in range(2)])
+        )
+        self.assertEqual(
+            self.left_appended_deque ** 4,
+            deques.Deque((num for num in self.items[::-1] for _ in range(4)))
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Chapter_3/Deques/deques.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+
+class Deque():
+    """
+    Implementation of a deque (double-ended queue) abstract data type. Items
+    can be added and removed from either side of the deque.
+
+    This implmentation has O(1) appends and pops from the right side and
+    O(n) appends and pops from the left side.
+    """
+    def __init__(self, items=None):
+        if items is None:
+            self.items = []
+        else:
+            if isinstance(items, list):
+                self.items = items
+            else:
+                try:
+                    self.items = [item for item in items]
+                except TypeError:
+                    raise
+
+    def __len__(self):
+        return len(self.items)
+
+    def append_right(self, item):
+        """Append item to the right end of the deque."""
+        self.items.append(item)
+
+    def append_left(self, item):
+        """Insert item on the left side of the deque."""
+        self.items.insert(0, item)
+
+    def pop_right(self):
+        """Returns the item on the right end of the deque."""
+        return self.items.pop()
+
+    def pop_left(self):
+        """Returns the item on the left end of the deque."""
+        return self.items.pop(0)
+
+    def __getitem__(self, position):
+        return self.items[position]
+
+    def __add__(self, other):
+        """Append all items of other unto the right side of the deque."""
+        try:
+            for item in other:
+                self.append_right(item)
+        except TypeError:
+            # other is not iterable
+            self.append_right(other)
+        return Deque(items=self.items)
+
+    def __radd__(self, other):
+        """Append all items of other unto the left side of the deque."""
+        try:
+            for item in reversed(other):
+                self.append_left(item)
+        except TypeError:
+            # other is not iterable
+            self.append_left(other)
+        return Deque(items=self.items)
+
+    def __repr__(self):
+        return "Deque({})".format(repr(self.items))
+
+    def __str__(self):
+        return repr(self)
+
+    def __bool__(self):
+        return bool(self.items)
+
+    def __mul__(self, n):
+        return Deque(self.items * n)
+
+    def __pow__(self, n):
+        """Every element in the deque is repeated n times."""
+        return Deque((repeat_item
+                      for item in self.items
+                      for repeat_item in [item] * n))
+
+    def __eq__(self, other):
+        try:
+            return self.items == other.items
+        except AttributeError:
+            return False
