3e24230e1c61470c4c314872ea5e5a30fe06423e
Jose Vargas <jpv.badilla@gmail.com>
2018-05-25 14:17:50 -0400

    Added tests for the append, index, pop, and insert methods.

--- Chapter_3/Linked_Lists/Tests/test_unordered_list.py
+++ Chapter_3/Linked_Lists/Tests/test_unordered_list.py
@@ -60,7 +60,7 @@ class TestUnorderedList(unittest.TestCase):
             self.assertEqual(actual_item, expected_item)
 
     def test_negative_indexing_of_linked_list(self):
-        for i in range(-self.num_elements, 0, -1):
+        for i in range(-self.num_elements, 0, 1):
              actual_item = self.unordered_list[i]
              expected_item = self.list_equivalent[i]
              self.assertEqual(actual_item, expected_item)
@@ -88,6 +88,86 @@ class TestUnorderedList(unittest.TestCase):
             for node in self.unordered_list[slice_object]
         ]
         self.assertEqual(sliced_list, self.list_equivalent[slice_object])
+
+    def test_append_method_of_linked_list(self):
+        value_to_append = 6
+        self.unordered_list.append(value_to_append)
+        self.list_equivalent.append(value_to_append)
+        list_of_values = [
+            node.get_item()
+            for node in self.unordered_list
+        ]
+        self.assertEqual(list_of_values, self.list_equivalent)
+
+
+    def test_index_method_of_linked_list(self):
+        """Test the .index(item) method."""
+        self.assertEqual(self.unordered_list.index(20), 4)
+
+    def test_index_method_raises_ValueError(self):
+        """
+        The .index(item) method should raise ValueError if `item` is not
+        in the list.
+        """
+        with self.assertRaises(ValueError):
+            self.unordered_list.index(50)
+
+    def test_item_received_from_pop_method(self):
+        self.assertEqual(self.unordered_list.pop(), 0)
+
+    def test_length_of_list_is_reduced_by_pop_method(self):
+        _ = self.unordered_list.pop()
+
+        predicted_length = self.num_elements - 1
+        actual_length = len(self.unordered_list)
+        self.assertTrue(actual_length == predicted_length)
+
+    def test_pop_raises_IndexError(self):
+        """IndexError should be raised when popping from an empty list."""
+        with self.assertRaises(IndexError):
+            self.empty_unordered_list.pop()
+
+    def test_pop_list_with_one_item(self):
+        other_list = ul.UnorderedList()
+        other_list.add(1)
+        _ = other_list.pop()
+        self.assertEqual(other_list, self.empty_unordered_list)
+
+    def test_insert_in_middle_of_list(self):
+        """
+        Insert method should work the same as for a normal python list.
+        """
+        value_to_insert = 6
+
+        # Create a new linked list that will be used for comparison.
+        other_list = ul.UnorderedList()
+        list_of_nums = [
+            num
+            for num in range(self.num_elements - 4, self.num_elements)
+        ]
+        list_of_nums.insert(2, value_to_insert)
+        for num in list_of_nums:
+            other_list.add(num)
+
+        # Add value in the middle of a linked list.
+        self.unordered_list.insert(2, value_to_insert)
+
+        self.assertEqual(self.unordered_list[:5], other_list)
+
+    def test_insert_into_linked_list_with_a_single_node(self):
+        """
+        Insert method should work the same as for a normal python list.
+        """
+        value_to_insert = 6
+
+        # Create a new unordered list to compare to.
+        test_list = ul.UnorderedList()
+        test_list.add(value_to_insert)
+        test_list.insert(1, value_to_insert * 2)
+
+        for num in (value_to_insert * 2, value_to_insert):
+            self.empty_unordered_list.add(num)
+        self.assertEqual(test_list, self.empty_unordered_list)
 
 
 if __name__ == "__main__":
